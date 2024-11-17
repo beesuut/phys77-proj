00802ea2266a341ac164049a1ab69b0b1aa1ce2d
beesuut <jdcheng06@gmail.com>
2024-11-16 19:35:26 -0800

    editing

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # phys77-proj
 Physics 77 Capstone Project
 
-Monde Carlo Simulation for an idealized (homogeneous and spherical) nuclear fission reactor.
+Monte Carlo Simulation for an idealized (homogeneous and spherical) nuclear fission reactor.
 
 Model:
 
 1. Neutrino travel, escape, and interactions (absorption, scattering, and fission)
 2. Criticality / Power production
 3. Antineutrino flux out of the reactor / Plutonium production
+
+# Notes on readability:
+
+Comments should be lowercase and begin with a space
+    '# example comment'
+
+Inline comments should be tabbed at least once
+
+'#%%' used as a comment to separate sections of code within a single file
+    Also allows for python file to be run in blocks (like a notebook)
---
+++ one_neutron_ex.py
@@ -0,0 +1,44 @@
+#%% working single particle model before monte-carlo
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import quad
+from pynverse import inversefunc
+
+#%% initial conditions
+
+# initialize a neutron with a random direction, energy, and location
+
+nenergy = 1     # ideally a random value between 5 ish different possibilities
+neutron = np.random.uniform(low=-1.0, high=1.0, size =(2,3))    # row 1: location; row 2: direction; columns for 3d
+
+# numbers for pure u238; nenergy = 1 MeV
+sf = 13     # mb fission
+si = 1300   # mb inelastic
+se = 4000   # mb elastic 
+
+# the total crossection is just the sum of the fission, absorbtion (inelastic) and scattering (elastic) crossections
+stot = 1   # sf+si+se    I tried this but it gave numbers too low, so I think something is off
+
+n = 1   # densisty = 19.05 #g/cm3 ^^same issue^^
+
+#%% probability of escape
+
+# for a particle density of "n" and a cross section of "s", the free path pdf is given by: 1/ns * e^-x/ns
+
+def pdf(x):
+    return (1/(n*stot)*np.exp(-x/(n*stot)))
+
+# integrate the pdf to get a cdf that goes from 0<y<1
+def cdf(x):
+    return quad(pdf, 0, x)[0]
+
+
+# from there we pick a random number from 0<y<1, and then we can use that as the y value for the cdf,
+# which then can tell us the distance the particle will travel
+
+inverse_cdf = inversefunc(cdf)
+
+tempvar = np.random.uniform(0,1)
+
+dist = inverse_cdf(tempvar)     # distance travelled before interaction
