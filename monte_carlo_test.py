ae1099519ca88cd46fbd9311f1e39f8b9937d6e0
beesuut <jdcheng06@gmail.com>
2024-11-18 13:09:03 -0800

    correct outputs

--- monte_carlo_test.py
+++ monte_carlo_test.py
@@ -20,20 +20,19 @@ sf = 0.013 * (10 ** (-24))  # cm^2 crossection fission
 si = 1.300 * (10 ** (-24))  # cm^2 crossection inelastic
 se = 4.000 * (10 ** (-24))  # cm^2 crossection elastic
 n = 4.98 * (10**22)  # atoms/cm^3
-stot = (
-    sf + si + se
-)  # the total crossection is just the sum of the crossections
+stot = (sf + si + se)  # the total crossection is just the sum of the crossections
 
 
 # initialize a neutron with a random direction, energy, and location
 
 nenergy = 1  # 1 is a fast/high energy neutron, all neutrons are made this way
-neutron = np.random.uniform(
-    low=0, high=1.0, size=(2, 3, count)
-)  # row 1: location; row 2: direction; multiple particles
-neutron[1, :] = neutron[1, :] / (
-    (np.sum((neutron[1, :]) ** 2)) ** 0.5
-)  # direction vector has length 1
+neutron = np.random.uniform(low=0, high=1.0, size=(2, 3, count))  # row 1: location; row 2: direction; multiple particles
+
+x, y, z = neutron[1, :] # component vectors
+vec = np.zeros(count) # direction vector
+for i in range(count):
+    vec[i] = (np.sum(x[i] ** 2 + y[i] ** 2 + z[i] ** 2)) ** 0.5 # magnitude of direction vector
+neutron[1, :] = neutron[1, :] / vec  # direction vector has length 1
 
 # define position in spherical coordinates
 rho = neutron[0, 0] * radiusofreactor
@@ -77,36 +76,28 @@ def escape(neut):
 
     dist = np.empty(count)
 
-    for i in range(count):
-        dist[i] = inverse_cdf(
-            np.random.uniform(0, 1)
-        )  # distance travelled before interaction
+    map(inverse_cdf, dist)  # distance travelled before interaction
 
     # check if particle escapes
 
-    neut[0, :] = np.add(
-        neut[0, :], dist * neut[1, :]
-    )  # change neutron position by dist in movement direction
+    neut[0, :] = np.add(neut[0, :], dist * neut[1, :])  # change neutron position by dist in movement direction
 
     pos = np.linalg.norm(neut[0, :], axis=0)  # find final position
 
     for i in range(count):
         if pos[i] <= radiusofreactor:  # particle didn't leave
             prob = np.random.uniform(0, stot * (10**24))
-            if prob < sf * (10**24):  # probability of fission
+            if prob < se * (10**24):  # probability of elastic collision
+                # append position and velocity into new list of neutrons; use recursion to run escape(newlist) (NEED TO DO)
+                # change energy if colliding with a light particle; requires newlist to have 4th dimension of energy; may need to redefine pdf(x) (NEED TO DO)
+                scount += 1
+            elif prob < (se + si) * (10**24):  # probability of inelastic collision
+                acount += 1
+            else:
                 # do a fission (NEED TO DO)
                 fcount += 1  # increase fission count
-            elif prob < (sf + se) * (
-                10**24
-            ):  # probability of elastic collision
-                # change energy if colliding with a light particle(NEED TO DO)
-                scount += 1
-                escape(neut)  # repeat until fission, absorption, or escape
-            else:
-                acount += 1
         else:
             ecount += 1
-
 
 
 escape(neutron)
