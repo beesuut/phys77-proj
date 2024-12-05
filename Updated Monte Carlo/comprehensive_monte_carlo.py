cab2826f21d8ee32d3ad1c37623c65fb326965a8
finneganwright <finnegan_wright@berkeley.edu>
2024-12-04 19:05:24 -0800

    Update comprehensive_monte_carlo.py
    commentary and integrating materials_mixture.py

--- Updated Monte Carlo/comprehensive_monte_carlo.py
+++ Updated Monte Carlo/comprehensive_monte_carlo.py
@@ -11,18 +11,19 @@ acount = 0      # absorption
 scount = 0      # scattering
 ecount = 0      # escape
 
-# numbers for pure u238; nenergy = 1 MeV
-sf = 0.013 * (10 ** (-24))      # cm^2 fission crossection
-sa = 1.300 * (10 ** (-24))      # cm^2 absorption crossection
-ss = 4.000 * (10 ** (-24))      # cm^2 scattering crossection
-stot = (sf + sa + ss)           # total crossection
-n = 4.98 * (10**22)             # atoms/cm^3
+# mixture percentages (of total volume)
+u238 = 0.25  # uranium-235 
+u235 = 0.25  # uranium-238
+boron = 0.25  # boron-10
+hw = 0.25  # heavy water
+
+mixture(u238, u235, boron, hw)  # outputs all needed atom totals and cross-sections
 
 nenergy_initial = np.ones(count)
 
 neutrons = neutrons(count, reactorradius, nenergy_initial) # initialize neutrons for monte carlo
 
-escape(neutrons, count, reactorradius, sa, ss, stot, n)
+escape(neutrons, count, reactorradius, sa, ss, stot, n)  # runs overall function with inputs, giving outputs printed below
 
 print('fission, absorption, scattering, escape')
 print(fcount, acount, scount, ecount)
