baca2b7a1cd1cfd040437148a8df773a5f6d7e10
finneganwright <finnegan_wright@berkeley.edu>
2024-12-12 15:43:32 -0800

    Update reactor_visualizations.py

--- Final Submission/Reactor Visualizations/reactor_visualizations.py
+++ Final Submission/Reactor Visualizations/reactor_visualizations.py
@@ -70,29 +70,6 @@ def boron(n):
     plt.savefig('boron.png')
     plt.show()
 
-# graphical function varying enrichment levels to see resulting neutron output and compare to criticality threshold, with neutrons and reactor radius as an input
-def criticality_enrichment(n, reactorradius):
-
-    # initiate empty list to be filled with neutron outputs, ideally approaching chain reaction levels
-    chain_reaction = []
-
-    # run through reactor function at enrichment levels 1% to 100% 
-    for i in range(1, 101):
-        fcount, acount, scount, ecount = reactor(n, 1-0.01*i, 0.01*i, 0, 0, reactorradius)
-        chain_reaction.append(fcount*2.5)    # multiply fission count by average neutrons produced per fission
-
-    # arrange percentages for x-axis
-    X = np.arange(1,101)
-    
-    plt.plot(X, chain_reaction, color = 'red')    # graph neutron output data
-    plt.hlines(n, 0, 100, color = 'black', linestyle = '--')    # graph horizontal line indicating threshold for criticality based on input neutrons
-    plt.title('Neutrons Produced From Fission - (with ' + str(n) + ' neutrons)')
-    plt.xlabel('% of core that is U-235')
-    plt.ylabel('# of neutrons produced')
-    
-    plt.savefig('criticality.png')
-    plt.show()
-
 # graphical function varying radius, in cm, to see resulting neutron output and compare to criticality threshold, with neutrons as an input
 def criticality_radius(n):
 
@@ -113,7 +90,7 @@ def criticality_radius(n):
     plt.xlabel('Radius of core, in cm')
     plt.ylabel('# of neutrons produced')
      
-    plt.savefig('criticality.png')
+    plt.savefig('criticality_radius_nr.png')
     plt.show()
 
 # graphical function varying distance from the reactor to see resulting number of antineutrinos
