ae56076ead3004d696b660a631b528ccbc052111
finneganwright <finnegan_wright@berkeley.edu>
2024-12-12 16:05:33 -0800

    Update reactor_visualizations_w_reflectivity.py

--- Final Submission/Visualizations/reactor_visualizations_w_reflectivity.py
+++ Final Submission/Visualizations/reactor_visualizations_w_reflectivity.py
@@ -26,14 +26,14 @@ def criticality_enrichment(n, reflectivity, reactorradius):
     plt.show()
 
 # graphical function varying radius, in cm, to see resulting neutron output and compare to criticality threshold, with neutrons as an input
-def criticality_radius_r(n):
+def criticality_radius_r(n, reflectivity):
     
     # initiate empty list to be filled with neutron outputs, ideally approaching chain reaction levels
     chain_reaction = []
     
     # run through reactor function at radii of 0.1 cm to 10 cm
     for i in range(1, 101):
-        fcount, acount, scount, ecount = reactor(n, 0.8, 0.2, 0, 0, 0.5, 0.1*i)
+        fcount, acount, scount, ecount = reactor(n, 0.8, 0.2, 0, 0, reflectivity, 0.1*i)
         chain_reaction.append(fcount*2.5)    # multiply fission count by average neutrons produced per fission
     
     # arrange radii for x-axis
@@ -41,7 +41,7 @@ def criticality_radius_r(n):
     
     plt.plot(X, chain_reaction, color = 'red')     # graph neutron output data
     plt.hlines(n, 0.1, 10, color = 'black', linestyle = '--')    # graph horizontal line indicating threshold for criticality based on input neutrons
-    plt.title('Neutrons Produced From Fission - (with ' + str(n) + ' neutrons)')
+    plt.title('Neutrons Produced From Fission - (with ' + str(n) + ' neutrons and ' + str(reflectivity) + ' reflectivity)')
     plt.xlabel('Radius of core, in cm')
     plt.ylabel('# of neutrons produced')
      
